13884ad6dd5225b774608a3204b921fa5e29881e
Bhasura <brian.desilva5@gmail.com>
2019-04-16 20:09:24 +1200

    Menu System v2.0
    This menu has 2 stages and one stage is linked to a small scene. It has music, loading screen, tutorial stage, stage 1 and options menu with volume control.

---
+++ IcenSpice Menu 2.0/Assets/Scripts/UIManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform mainMenu, stage1, stage2, loadingScreen;
+    public GameObject optionsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, 0.25f);
+    }
+
+    // Update is called once per frame
+    //in main menu
+    public void goToStage1()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, -529), 0.25f);
+        stage1.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+    public void showOptionsPanel()
+    {
+        optionsPanel.SetActive(true);
+    }
+    public void hideOptionsPanel()
+    {
+        optionsPanel.SetActive(false);
+    }
+    //in stage 1
+    public void goToStage2()
+    {
+        stage1.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        stage2.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void s1MainMenu()
+    {
+        stage1.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void s1Start()
+    {
+        stage1.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        loadingScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    //in stage 2
+    public void s2GoToStage1()
+    {
+        stage2.DOAnchorPos(new Vector2(1100, 0), 0.25f);
+        stage1.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void s2MainMenu()
+    {
+        stage2.DOAnchorPos(new Vector2(1100, 0), 0.25f);
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+}
