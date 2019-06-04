7edb10f57e9ecb47972298f76128a2a5b9879e38
Bhasura <brian.desilva5@gmail.com>
2019-06-04 17:37:48 +1200

    1

---
+++ FinalGame/Assets/Editor/BuyShopItemTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class BuyShopItemTest 
+{
+    [Test]
+    public void BuyItemShop_Test()
+    {
+        var shopBuy = new shopTests();
+        //test 1
+        var itemPrice = 900;
+        var name = "Ice Cream";
+        var expectedResult = 100;
+
+
+        //Act
+        var balance = shopBuy.buyItem(itemPrice, name);
+
+        //Assert
+        Assert.That(balance, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void returnItem_Test()
+    {
+        var shopBuy = new shopTests();
+        var itemPrice = 1000;
+        var name = "Ice Cream";
+       
+        var itemNumber = 0;
+        var expectedResult = "Ice Cream";
+
+        //Act
+        var balance = shopBuy.buyItem(itemPrice, name);
+        var item = shopBuy.inventoryName(itemNumber);
+
+        //Assert
+        Assert.That(item, Is.EqualTo(expectedResult));
+    }
+}
---
+++ FinalGame/Assets/Editor/shopTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shopTests : MonoBehaviour
+{
+    public int gold = 1000;
+    ArrayList inventory = new ArrayList();
+
+    public int buyItem(int itemPrice, string name)
+    {
+        if(gold >= itemPrice)
+        {
+            gold -= itemPrice;
+            inventory.Add(name);
+        }
+        else
+        {
+            return gold;
+        }
+
+        return gold;
+    }
+
+    public string inventoryName(int slot)
+    {
+        string itemName;
+        if (inventory[slot] == null)
+        {
+            itemName = "empty";
+        }
+        else
+        {
+            itemName = (string)inventory[slot];
+        }
+        
+        return itemName;
+    }
+}
