a56b987edeef59dc511f2f416831cf6886ef4643
Bhasura <brian.desilva5@gmail.com>
2019-06-04 20:06:28 +1200

    Cleaned up solo UI

--- FinalGame/Assets/MenuScripts/Scripts/PlayerHp.cs
+++ FinalGame/Assets/MenuScripts/Scripts/PlayerHp.cs
@@ -9,14 +9,18 @@ public class PlayerHp : MonoBehaviour
     
     public static int tempGold = 0;
     GameObject gText;
+    GameObject hText;
     public void Start()
     {
         gText = GameObject.Find("GoldTextMesh");
         gText.GetComponent<TextMeshProUGUI>().SetText(tempGold + " Gold Earned");
-       
+        hText = GameObject.Find("Health");
+        hText.GetComponent<TextMeshProUGUI>().SetText("Health : " + health);
+
     }
     public void Update()
     {
         gText.GetComponent<TextMeshProUGUI>().SetText(tempGold + " Gold Earned");
+        hText.GetComponent<TextMeshProUGUI>().SetText("Health : " + health);
     }
 }
